632a8199e523be46ab471819f81a2a3b522ad016
Jackwhitener <jackwhitener1@gmail.com>
2018-09-25 14:17:52 -0400

    Added a negative test

--- test_addition.rb
+++ test_addition.rb
@@ -14,5 +14,10 @@ class My_test < Minitest::Test
 	def test_three
 		assert_equal(3,add(1,2))
 	end
-
+	def test_negative
+		arg1 = -5	
+		arg2 = 15
+		results = add(arg1,arg2)
+		assert_equal(10,results)
+	end
 end
